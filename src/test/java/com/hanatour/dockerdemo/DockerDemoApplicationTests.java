368d0b0512d774d0de94a0506360073f81ac0e69
HG086 <hukim@hanatour.com>
2021-11-26 13:01:50 +0900

    init

---
+++ src/test/java/com/hanatour/dockerdemo/DockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanatour.dockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
