09513f22db5c65b090d4274fa1ba149bcb4708bc
HG086 <hukim@hanatour.com>
2021-11-26 19:37:51 +0900

    test

--- script/start.sh
+++ script/start.sh
@@ -1,1 +1,2 @@
-nohup java -Dserver.port=80 -jar /home/ec2-user/test/build/libs/dockerDemo-0.0.1-SNAPSHOT.jar
+nohup java -Dserver.port=80 -jar /home/ec2-user/test/build/libs/dockerDemo-0.0.1-SNAPSHOT.jar 2>&1 \
+      & sleep 1
--- script/stop.sh
+++ script/stop.sh
@@ -1,4 +1,4 @@
-PID=`ps -ef | grep java | awk '{ print $2 }'`
+PID=`ps -ef | grep java | grep Demo | awk '{ print $2 }'`
 
 if [ -z $PID ]; then
   echo -e "That process doesn't exist."
