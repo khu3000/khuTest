8a4c49349bcae1e1d04f038f4747eefcb3314fcf
HG086 <hukim@hanatour.com>
2021-12-13 23:28:16 +0900

    test

--- src/main/java/com/hanatour/dockerdemo/HelloController.java
+++ src/main/java/com/hanatour/dockerdemo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello, Spring boot!";
+        return "Hello, Spring boot! docker-demo";
     }
 
 }
