85abac3b6015c03d15de5196bfe3df5b1a28b574
HG086 <hukim@hanatour.com>
2021-12-31 16:17:23 +0900

    test

--- script/docker-pull.sh
+++ script/docker-pull.sh
@@ -1,2 +1,2 @@
-sudo docker pull 293944524863.dkr.ecr.ap-northeast-2.amazonaws.com/jenkins-test:latest
-sudo docker create -p 80:8080 --name docker-demo 293944524863.dkr.ecr.ap-northeast-2.amazonaws.com/jenkins-test
+sudo docker pull 695809562949.dkr.ecr.ap-northeast-2.amazonaws.com/hnt-dcr/test/dev:latest
+sudo docker create -p 8080:8080 --name docker-demo 695809562949.dkr.ecr.ap-northeast-2.amazonaws.com/hnt-dcr/test/dev
