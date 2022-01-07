789b894d512a707a359c50ea612eddee80ef8b58
HG086 <hukim@hanatour.com>
2022-01-07 17:08:00 +0900

    test

--- script/docker-pull.sh
+++ script/docker-pull.sh
@@ -1,2 +1,3 @@
+sudo $(aws ecr get-login --no-include-email --region ap-northeast-2)
 sudo docker pull 695809562949.dkr.ecr.ap-northeast-2.amazonaws.com/hnt-dcr/test/dev:latest
 sudo docker create -p 8080:8080 --name docker-demo 695809562949.dkr.ecr.ap-northeast-2.amazonaws.com/hnt-dcr/test/dev
