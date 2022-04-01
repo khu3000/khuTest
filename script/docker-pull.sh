93ac2a89541819bfb65cf5d5f6f4681a7548224e
김현욱 <hukim@hanatour.com>
2022-04-02 01:05:03 +0900

    Update script/docker-pull.sh

--- script/docker-pull.sh
+++ script/docker-pull.sh
@@ -1,2 +1,2 @@
 sudo $(aws ecr get-login --no-include-email --region ap-northeast-2)
-sudo docker pull 695809562949.dkr.ecr.ap-northeast-2.amazonaws.com/hnt-dcr/test/dev:108
+sudo docker pull 695809562949.dkr.ecr.ap-northeast-2.amazonaws.com/hnt-dcr/test/dev:latest
