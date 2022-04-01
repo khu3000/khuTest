3bf121eee16dbc8533ba9c4a7a16935f8c184952
김현욱 <hukim@hanatour.com>
2022-04-02 01:29:30 +0900

    Update docker-run.sh

--- script/docker-run.sh
+++ script/docker-run.sh
@@ -1,1 +1,1 @@
-sudo docker run -p 8080:8080 --name docker-demo 695809562949.dkr.ecr.ap-northeast-2.amazonaws.com/hnt-dcr/test/dev
+sudo docker run -d -p 8080:8080 --name docker-demo 695809562949.dkr.ecr.ap-northeast-2.amazonaws.com/hnt-dcr/test/dev
