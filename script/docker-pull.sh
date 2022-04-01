f1593be4f1b267c32757b9add6a942e98b5c72c9
김현욱 <hukim@hanatour.com>
2022-04-02 00:58:18 +0900

    Update script/docker-pull.sh, script/docker-run.sh files

--- script/docker-pull.sh
+++ script/docker-pull.sh
@@ -1,3 +1,2 @@
 sudo $(aws ecr get-login --no-include-email --region ap-northeast-2)
 sudo docker pull 695809562949.dkr.ecr.ap-northeast-2.amazonaws.com/hnt-dcr/test/dev:108
-sudo docker create -p 8080:8080 --name docker-demo 695809562949.dkr.ecr.ap-northeast-2.amazonaws.com/hnt-dcr/test/dev
--- script/docker-run.sh
+++ script/docker-run.sh
@@ -1,1 +1,1 @@
-sudo docker start docker-demo
+sudo docker run -p 8080:8080 --name docker-demo 695809562949.dkr.ecr.ap-northeast-2.amazonaws.com/hnt-dcr/test/dev
