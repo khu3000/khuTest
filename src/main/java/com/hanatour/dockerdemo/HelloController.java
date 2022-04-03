e58b1306876c3e2c8ae541249d71fc0e869ff49e
김현욱 <hukim@hanatour.com>
2022-04-03 21:43:23 +0900

    Update src/main/java/com/hanatour/dockerdemo/HelloController.java

--- src/main/java/com/hanatour/dockerdemo/HelloController.java
+++ src/main/java/com/hanatour/dockerdemo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello, Spring boot! docker-demo 0402 aa bb cc";
+        return "Hello, Spring boot! docker-demo 040333";
     }
 
 }
