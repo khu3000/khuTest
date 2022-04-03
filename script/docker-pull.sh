619586f4f9c7251f2821b0608713e14357f6046d
김현욱 <hukim@hanatour.com>
2022-04-03 21:38:32 +0900

    Update script/docker-pull.sh, script/docker-run.sh files

--- script/docker-pull.sh
+++ script/docker-pull.sh
@@ -1,2 +1,7 @@
-sudo $(aws ecr get-login --no-include-email --region ap-northeast-2)
-sudo docker pull 695809562949.dkr.ecr.ap-northeast-2.amazonaws.com/hnt-dcr/test/dev:latest
+#!/bin/sh
+source /home/ec2-user/test/script/config.sh
+
+
+$(aws ecr get-login --no-include-email --region ap-northeast-2)
+#docker pull 695809562949.dkr.ecr.ap-northeast-2.amazonaws.com/hnt-dcr/test/dev:latest
+docker pull 695809562949.dkr.ecr.ap-northeast-2.amazonaws.com/hnt-dcr/test/dev:${DOCKER_VERSION}
--- script/docker-run.sh
+++ script/docker-run.sh
@@ -1,1 +1,5 @@
-sudo docker run -d -p 8080:8080 --name docker-demo 695809562949.dkr.ecr.ap-northeast-2.amazonaws.com/hnt-dcr/test/dev
+#!/bin/sh
+source /home/ec2-user/test/script/config.sh
+
+
+docker run -d -p 8080:8080 --name docker-demo 695809562949.dkr.ecr.ap-northeast-2.amazonaws.com/hnt-dcr/test/dev:${DOCKER_VERSION}
